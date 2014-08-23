a67772d96245a62937ecd16aba8688bee99d2f9c
Evan Czaplicki <info@elm-lang.org>
2014-08-23 15:28:21 +0200

    cosmetic: be more consistent with indentation

--- Automaton.elm
+++ Automaton.elm
@@ -1,11 +1,12 @@
-module Automaton ( pure, state, hiddenState
-                 , run, step
-                 , (>>>), (<<<)
-                 , branch, pair, merge
-                 , first, second
-                 , combine, loop
-                 , count, average
-                 ) where
+module Automaton
+    ( pure, state, hiddenState
+    , run, step
+    , (>>>), (<<<)
+    , branch, pair, merge
+    , first, second
+    , combine, loop
+    , count, average
+    ) where
 
 {-| This library is for structuring reactive code. The key concepts come
 directly from [Arrowized FRP][afrp]. It is not yet clear how
@@ -38,7 +39,8 @@ import Signal ( lift, foldp, Signal )
 import List (..)
 import Maybe ( Maybe(Just, Nothing) )
 
-data Automaton a b = Step (a -> (Automaton a b, b))
+data Automaton a b =
+    Step (a -> (Automaton a b, b))
 
 {-| Run an automaton on a given signal. The automaton steps forward whenever the
 input signal updates.
@@ -50,7 +52,8 @@ input signal updates.
 run : Automaton i o -> o -> Signal i -> Signal o
 run auto base inputs =
   let step a (Step f, _) = f a
-  in  lift (\(x,y) -> y) (foldp step (auto,base) inputs)
+  in
+      lift (\(x,y) -> y) (foldp step (auto,base) inputs)
 
 {-| Step an automaton forward once with a given input.
 
@@ -73,9 +76,11 @@ to build a ship out of wood and a way to crash a ship and create a wreck.
 -}
 (>>>) : Automaton i inner -> Automaton inner o -> Automaton i o
 (>>>) f g =
-  Step <| \a -> let (f', b) = step a f
-                    (g', c) = step b g
-                 in ((>>>) f' g', c)
+  Step <| \a ->
+    let (f', b) = step a f
+        (g', c) = step b g
+    in
+        (f' >>> g', c)
 
 {-| Compose two automatons into a pipeline. For example, lets say we have a way
 to build a ship out of wood and a way to crash a ship and create a wreck.
@@ -88,9 +93,11 @@ to build a ship out of wood and a way to crash a ship and create a wreck.
 -}
 (<<<) : Automaton inner o -> Automaton i inner -> Automaton i o
 (<<<) g f =
-  Step <| \a -> let (f', b) = step a f
-                    (g', c) = step b g
-                 in ((<<<) g' f', c)
+  Step <| \a ->
+    let (f', b) = step a f
+        (g', c) = step b g
+    in
+        (g' <<< f', c)
 
 {-| Take a single input and branch it out into two different results.
 
@@ -102,9 +109,11 @@ to build a ship out of wood and a way to crash a ship and create a wreck.
 -}
 branch : Automaton i o1 -> Automaton i o2 -> Automaton i (o1, o2)
 branch f g =
-  Step <| \a -> let (f', b) = step a f
-                    (g', c) = step a g
-                 in (branch f' g', (b, c))
+  Step <| \a ->
+    let (f', b) = step a f
+        (g', c) = step a g
+    in
+        (branch f' g', (b, c))
 
 {-| Combine two independent automatons. The new automaton takes a pair of
 inputs and produces a pair of outputs. In this case we convert two separate
@@ -118,9 +127,11 @@ values into two separate piles of wood:
 -}
 pair : Automaton i1 o1 -> Automaton i2 o2 -> Automaton (i1, i2) (o1, o2)
 pair f g = 
-  Step <| \(a, b) -> let (f', c) = step a f
-                         (g', d) = step b g
-                      in (pair f' g', (c, d))
+  Step <| \(a, b) ->
+    let (f', c) = step a f
+        (g', d) = step b g
+    in
+        (pair f' g', (c, d))
 
 {-| Create an automaton that takes in a tuple and returns a tuple, but only
 transform the *first* thing in the tuple.
@@ -137,8 +148,10 @@ It may be helpful to know about the following equivalence:
 -}
 first : Automaton i o -> Automaton (i, extra) (o, extra)
 first auto = 
-  Step <| \(i, ex) -> let (f, o) = step i auto
-                       in (first f, (o, ex))
+  Step <| \(i, ex) ->
+    let (f, o) = step i auto
+    in
+        (first f, (o, ex))
 
 {-| Create an automaton that takes in a tuple and returns a tuple, but only
 transform the *first* thing in the tuple.
@@ -155,8 +168,10 @@ It may be helpful to know about the following equivalence:
 -}
 second : Automaton i o -> Automaton (extra, i) (extra, o)
 second auto = 
-  Step <| \(ex, i) -> let (f, o) = step i auto
-                       in (second f, (ex, o))
+  Step <| \(ex, i) ->
+    let (f, o) = step i auto
+    in
+        (second f, (ex, o))
 
 {-| Create an automaton that takes a branched input and merges it into a single
 output.
@@ -191,16 +206,20 @@ benefit of loop is that you can add state to *any* automaton. We used
 -}
 loop : state -> Automaton (i,state) (o,state) -> Automaton i o
 loop state auto =
-    Step <| \input -> let (auto', (output,state')) = step (input,state) auto
-                      in  (loop state' auto', output)
+  Step <| \input ->
+    let (auto', (output,state')) = step (input,state) auto
+    in 
+        (loop state' auto', output)
 
 {-| Combine a list of automatons into a single automaton that produces a
 list.
 -}
 combine : [Automaton i o] -> Automaton i [o]
 combine autos =
-  Step <| \a -> let (autos', bs) = unzip (map (step a) autos)
-                in  (combine autos', bs)
+  Step <| \a ->
+    let (autos', bs) = unzip (map (step a) autos)
+    in
+        (combine autos', bs)
 
 {-| Create an automaton with no memory. It just applies the given function to
 every input.
@@ -229,8 +248,10 @@ increments the state on every step.
 -}
 state : b -> (a -> b -> b) -> Automaton a b
 state s f =
-    Step <| \x -> let s' = f x s
-                  in  (state s' f, s')
+  Step <| \x ->
+    let s' = f x s
+    in
+        (state s' f, s')
 
 {-| Create an automaton with hidden state. Requires an initial state and a
 step function to step the state forward and produce an output.
@@ -248,33 +269,43 @@ worry about that as an outside observer.
 -}
 hiddenState : s -> (i -> s -> (o,s)) -> Automaton i o
 hiddenState s f =
-    Step <| \x -> let (s',out) = f x s
-                  in  (hiddenState out f, s')
+  Step <| \x ->
+    let (s',out) = f x s
+    in
+        (hiddenState out f, s')
 
 {-| Count the number of steps taken. -}
 count : Automaton a Int
 count = state 0 (\_ c -> c + 1)
 
 type Queue t = ([t],[t])
+
 empty = ([],[])
+
 enqueue x (en,de) = (x::en, de)
-dequeue q = case q of
-              ([],[]) -> Nothing
-              (en,[]) -> dequeue ([], reverse en)
-              (en,hd::tl) -> Just (hd, (en,tl))
+
+dequeue q =
+  case q of
+    ([],[]) -> Nothing
+    (en,[]) -> dequeue ([], reverse en)
+    (en,hd::tl) -> Just (hd, (en,tl))
 
 {-| Computes the running average of the last `n` inputs. -}
 average : Int -> Automaton Float Float
 average k =
-  let step n (ns,len,sum) =
-          if len == k then stepFull n (ns,len,sum)
-                      else ((sum+n) / (toFloat len+1), (enqueue n ns, len+1, sum+n))
+  let step n (ns, len, sum) =
+          if len == k
+            then stepFull n (ns,len,sum)
+            else ((sum+n) / (toFloat len+1), (enqueue n ns, len+1, sum+n))
+
       stepFull n (ns,len,sum) =
           case dequeue ns of
             Nothing -> (0, (ns,len,sum))
-            Just (m,ns') -> let sum' = sum + n - m
-                            in ((sum' / toFloat len), (enqueue n ns', len, sum'))
-  in  hiddenState (empty,0,0) step
+            Just (m,ns') ->
+              let sum' = sum + n - m
+              in  ((sum' / toFloat len), (enqueue n ns', len, sum'))
+  in
+      hiddenState (empty,0,0) step
 
 
 {-- TODO(evancz): See the following papers for ideas on how to make this
